1d48e789129757d947e24ebe19dc78f60d0749b5
Tejas <tejas.patiltp2802@gmail.com>
2022-03-07 22:00:35 +0530

    React

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return(
+        <React.Fragment>
+            <hr/>
+            <center>
+                <h3>&copy; Tejas Patil</h3>
+            </center>
+        </React.Fragment>
+    )
+}
+export default Footer
---
+++ src/component/Home/QuickSearch.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+import './QuickSearch.css'
+
+class QuickSearch extends Component{
+    render(){
+        return(
+            <h1>Quick Page</h1>
+        )
+    }
+}
+
+export default QuickSearch 
