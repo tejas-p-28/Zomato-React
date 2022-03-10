fab0ac5feac836b0fbfaaf2298b3f0c146696f73
Tejas <tejas.patiltp2802@gmail.com>
2022-03-10 14:55:33 +0530

    React

---
+++ src/component/Listing/listing.js
@@ -0,0 +1,38 @@
+import React,{Component} from 'react';
+import axios from 'axios';
+import './Listing.css';
+import ListingDisplay from './listingDisplay'
+
+const restUrl = "https://zomatotejas.herokuapp.com/restaurants?meal_id=";
+
+class Listing extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            restaurantList:''
+        };
+    }
+    render(){
+        return(
+            <>
+                <div className="row">
+                    <div id="mainListing">
+                        <div id="filter">
+                            <center>
+                                <h3>Filter</h3>   
+                            </center>    
+                        </div>
+                        <ListingDisplay listData={this.state.restaurantList}/>
+                    </div>
+                </div>
+            </>
+        )
+    }
+    componentDidMount(){
+        let mealid = this.props.match.params.mealId;
+        axios.get(`${restUrl}${mealid}`)
+        .then((res) => {this.setState({restaurantList:res.data})})
+    }
+}
+
+export default Listing
