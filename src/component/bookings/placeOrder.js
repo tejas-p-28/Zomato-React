2f68528def0042699c7127483afcae7a6bf0a327
Tejas <tejas.patiltp2802@gmail.com>
2022-03-29 16:42:00 +0530

    Zomato

--- src/component/bookings/placeOrder.js
+++ src/component/bookings/placeOrder.js
@@ -1,12 +1,15 @@
 import React,{Component} from 'react';
 import './placeOrder.css';
 
+const url="https://zomatotejas.herokuapp.com/menuItem";
+const postData="https://zomatotejas.herokuapp.com/orders"
+
 class placeOrder extends Component {
     constructor(props){
         super(props);
 
         this.state={
-            id:Math.floor(Math.random()*1000000000),
+            id:Math.floor(Math.random()*100000),
             hotel_name:this.props.match.params.restName,
             name:'',
             email:'',
@@ -16,10 +19,109 @@ class placeOrder extends Component {
             menuItem:''
         }
     }
+
+    handleChange = (event) => {
+        this.setState({[event.target.name]:event.target.value})
+    }
+
+    checkout = () => {
+        let obj = this.state;
+        obj.menuItem = sessionStorage.getItem('menu');
+        fetch(postData,{
+            method:'POST',
+            headers:{
+                'accept':'application/json',
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify(obj)
+        })
+        .then(this.props.history.push('/viewOrders'))
+    }
+
+    renderMenu = (data) => {
+        if(data){
+            return data.map((item) => {
+                return(
+                    <div className="orderItems" key={item.menu_id}>
+                        <img src={item.menu_image} alt={item.menu_name}/>
+                        <center>
+                            <h3>{item.menu_name}</h3>
+                            <h4>Rs. {item.menu_price}</h4>
+                        </center>
+                    </div>
+                )
+            })
+        }
+    }
+
     render(){
+        console.log(this.state)
         return(
-            <h1>Place Order</h1>
+            <>
+                <div className="container">
+                    <hr/>
+                    <div className="panel panel-primary">
+                        <div className="panel-heading">
+                            <h3>Your order from Restaurant {this.props.match.params.restName}</h3>
+                        </div>
+                        <div className="panel-body">
+                            <div className="row">
+                                <div className="form-group col-md-6">
+                                    <label for="fname">Name</label>
+                                    <input id="fname" name="name" className="form-control" value={this.state.name} onChange={this.handleChange}/>
+                                </div>
+                                <div className="form-group col-md-6">
+                                    <label for="email">Email</label>
+                                    <input id="email" name="email" className="form-control" value={this.state.email} onChange={this.handleChange}/>
+                                </div>
+                                <div className="form-group col-md-6">
+                                    <label for="phone">Phone</label>
+                                    <input id="phone" name="phone" className="form-control" value={this.state.phone} onChange={this.handleChange}/>
+                                </div>
+                                <div className="form-group col-md-6">
+                                    <label for="address">Address</label>
+                                    <input id="address" name="address" className="form-control" value={this.state.address} onChange={this.handleChange}/>
+                                </div>
+                            </div>
+                            {this.renderMenu(this.state.menuItem)}
+                            <div className="row">
+                                <div className="col-md-12">
+                                    <h2>Total Price is Rs. {this.state.cost}</h2>
+                                    
+                                </div>
+                                
+                            </div>
+                            <button className="btn btn-success" onClick={this.checkout}>Submit</button>
+                        </div>
+                    </div>
+                </div>
+            </>
         )
+    }
+    componentDidMount(){
+        let menuItem = sessionStorage.getItem('menu')
+        let orderId = [];
+        menuItem.split(',').map((item) => {
+            orderId.push(parseInt(item));
+            return 'ok'
+        })
+        fetch(url,{
+            method:'POST',
+            headers:{
+                'accept':'application/json',
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify(orderId)
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            let totalPrice = 0;
+            data.map((item) => {
+                totalPrice += parseFloat(item.menu_price)
+                return 'ok'
+            })
+            this.setState({cost: totalPrice, menuItem:data})
+        })
     }
 }
 
--- src/component/bookings/viewOrder.js
+++ src/component/bookings/viewOrder.js
@@ -1,10 +1,26 @@
 import React,{Component} from 'react';
+import axios from 'axios';
+import OrderDisplay from './orderDisplay';
+
+const url = "https://zomatotejas.herokuapp.com/orders";
 
 class viewOrder extends Component {
+    constructor(props){
+        super(props);
+
+        this.state={
+            orders:''
+        }
+    }
     render(){
         return(
-            <h1>View Order</h1>
+            <>
+                <OrderDisplay orderData={this.state.orders}/>
+            </>
         )
+    }
+    componentDidMount(){
+        axios.get(`${url}`).then((res) => {this.setState({orders:res.data})})
     }
 }
 
